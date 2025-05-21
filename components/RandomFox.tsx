c6adcee724f248092d4dc0d40c233a3e94f698dc
hugo-04 <edseldvegavega@gmail.com>
2025-05-21 01:05:06 -0500

    se mejora el componente de react

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,51 +1,52 @@
-import Image from 'next/image'
-import { useRef, useEffect ,useState } from 'react'
-import type { JSX } from 'react'
+import Image, { type ImageProps } from 'next/image';
+import { useRef, useEffect, useState } from 'react';
+import type { JSX } from 'react';
 
-type Props = {
-    image: string
-    alt?: string
-}
+type LazyImageProps = {
+    src: string;
+    alt: string;
+};
 
-export const RandomFox = ({
-    image,
-    alt = 'Random fox',
+type Props = LazyImageProps & Omit<ImageProps, 'src' | 'alt'>;
+
+export const LazyImage = ({
+    src,
+    alt,
+    ...imgProps
 }: Props): JSX.Element => {
+
     const node = useRef<HTMLImageElement>(null);
-    const [Src, setSrc] = useState("data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=");
+    
+    const [currentSrc, setCurrentSrc] = useState(
+        "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+    );
 
     useEffect(() => {
-        //nuevo observador
-        const observador = new IntersectionObserver((entries) => {
-            entries.forEach(entry => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
                 if (entry.isIntersecting) {
-                    setSrc(image)
+                    setCurrentSrc(src);
                 }
             });
         });
 
-        //observador node
         if (node.current) {
-            observador.observe(node.current);
+            observer.observe(node.current);
         }
 
-        //desconectar
         return () => {
-            observador.disconnect()
-        }
-    }, [image]);
+            observer.disconnect();
+        };
+    }, [src]);
 
     return (
-        <div className="rounded overflow-hidden">
+        <div className="rounded overflow-hidden relative">
             <Image
                 ref={node}
-                src={Src}
+                src={currentSrc}
                 alt={alt}
-                width={320}
-                height={320}
-                className="rounded bg-gray-300"
-                priority
+                {...imgProps}
             />
         </div>
     );
-}
+};
